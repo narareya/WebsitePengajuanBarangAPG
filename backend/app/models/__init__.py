793c707c2d3e619130ecf4ae3fcde1925e62769c
narareya <nomnomhiranami@gmail.com>
2026-08-05 11:49:48 +0700

    activity table log

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -4,3 +4,4 @@ from app.models.request import RequestModel
 from app.models.request_detail import RequestDetail
 from app.models.product import Product
 from app.models.token_blacklist import TokenBlacklist
+from app.models.activity_log import ActivityLog
